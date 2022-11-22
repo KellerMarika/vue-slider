330a889c3e0d287238262f793b3646bb9dc12775
KellerMarika <marikakeller92@gmail.com>
2022-11-23 00:41:13 +0100

    non riesco a sfruttare l'aggiornamento automatico del valore di isPaused al momento del mouseover per modificare lo stato dell'autoplay

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
 /* destrutturing */
 const { createApp } = Vue;
-
 const app = createApp({
 
-    /***** DATA ****************************************************************************************/
+    /***** DATA **************************************/
     data() {
         return {
             /* current index dovrà sempre riferirsi alla posizione dell'oggetto[] nell'array slides.*/
             current_Index: 0,
-            a: 0,
+            isPaused: false,
             slides: [
                 {
                     image: 'img/01.webp',
@@ -32,16 +31,10 @@ const app = createApp({
                     text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
                 }
             ],
-
-
         };
-
     },
-
-    /***** METHODS ****************************************************************************************/
+    /***** METHODS *********************************/
     methods: {
-
-
         /* INIZIO FUNZIONE NEXT  ****/
         nextSlide() {
             // è uguale a scrivere if(this.current_Index>slides.lenght) 
@@ -53,7 +46,6 @@ const app = createApp({
                 this.current_Index++;
             }
         },
-
         /* INIZIO FUNZIONE PREVIEW  ****/
         prevSlide() {
 
@@ -64,90 +56,46 @@ const app = createApp({
                 this.current_Index--;
             }
         },
-
         /* FUNZIONE THUMBNAIL CLICK (selectSlide) */
         //l'argomento di questa funzione deve essere lìindice della thumbnail cliccata
         selectThumbnail_onClick(clickedThumbnil_Index) {
             this.current_Index = clickedThumbnil_Index;
-            /*console.log(current_Index, clickedThumbnil_Index);______perchè mi dai errore?? ti odio Vue */
+            /*     console.log(this.current_Index) era il this. (ti odio comunque) */
+        },
 
-            /*    
-            //aggiungo classe alla thumb cliccata
-        this.classList.add("active");
-            
-            //disattiva tutte le thumbnail con dataset diverso da quello della thumb appena cliccata
-                disactiveOldThumbnails()
-            
-                //sostituzione slide
-                replaceSlide() 
-                 */
-        }
+        /* FUNZIONE STOP AUTOPLAY*/
+        stopAutoplay() {
+            console.log(this.isPaused);
+            return this.isPaused = true;
+        },
+
+        /* FUNZIONE AUTOPLAY*/
+        autoplay(callbackfunction) {
+            let autoplay_interval
+
+            if (!this.isPaused) {
+                autoplay_interval = setInterval(callbackfunction, 3000);
+                console.log(this.isPaused)
+            } else {
+                clearInterval(autoplay_interval);
+                console.log(this.isPaused)
+            }
+        },
+
+
     },
-
-    /***** MOUNTED ****************************************************************************************/
+    /***** MOUNTED ****************************************/
     mounted() {
+        this.autoplay(this.nextSlide),
+           /*  setInterval(this.autoplay, 3000), */
+     
         console.log("mounted called");
     }
-
 }).mount('#app');
 
+/* const slider_El = document.querySelector(".slider-wrapper");
+console.log(slider_El)
 
+slider_El.addEventListener("mouseover", () => {
 
-/********* FUNZIONE GENERICA SWITCH CALLBACKFUNCTION DI FUNZIONE ESTERNA  *********************/
-/* 
-//conta i click di revertBtn
-let revertCounter = 0
-//conta i click di playBtn
-let playBtnCounter = 0
-//callbackfunction è argomento di autoplay() e di default ha valore funzione nextSlide
-//nella funzione switchCallbackFunction il suo valore cambia in base al numero dei click
-let callbackfunction = nextSlide;
-
-//aggiungo add event listner al bottone(dovevo farlo sopra ma mi fa comodo tenere sott'occhio questi passaggi in maniera compatta)
-
-//se devo passare degli argomenti ad una funzione che viene invocata con un evento, utilizzo una arrowfunction per invocare la funzione nominale, senò dovrei scriverla così:
-
-// revertBtn_El.addEventListener("click", switchCallbackFunction);
-//switchCallbackFunction(nextSlide, prevSlide) 
-revertBtn_El.addEventListener("click", () => { switchCallbackFunction(nextSlide, prevSlide) });
-
-
-function switchCallbackFunction(callbackfunction1, callbackfunction2) {
-
-    revertCounter++
-    console.log("switchCallbackFunction revertCounter= ", revertCounter);
-    //QUI AVVIENE LA MAGIA
-    //se nel momento in cui clicco il revertBtn il playbtn è stato cliccato una sola volta, significa che la funzione autoplay() è nella prima if (modalità play) e ha il playBtnCounter dispari. 
-    if (playBtnCounter % 2 !== 0) {
-        //occorre invocare una sola volta la funzione autoplay, incrementare il playbtncounter di 1 e ripristinare la "condizione della seconda if (modalità pausa).
-        autoplay(callbackfunction);
-    }
-
-    if (revertCounter % 2 === 0) {
-        callbackfunction = callbackfunction1
-    } else {
-        callbackfunction = callbackfunction2
-    }
-    console.log("switchCallbackFunction revertCounter= ", revertCounter, callbackfunction);
-    return (callbackfunction);
-}
-
-//aggiungo add event listner al bottone(dovevo farlo sopra ma mi fa comodo tenere sott'occhio questi passaggi in maniera compatta)
-playBtn_El.addEventListener("click", () => { autoplay(callbackfunction) });
-// autoplay(callbackfunction)
- 
-let autoplay_interval
-
-function autoplay(callbackfunction) {
-
-    playBtnCounter++
-    console.log(playBtnCounter, "autoplay   callbackfunction is=", callbackfunction);
-    if (playBtnCounter % 2 !== 0) {
-        playBtn_El.innerHTML = playBtnIcons[1];
-        autoplay_interval = setInterval(callbackfunction, 3000);
-    } else {
-        playBtn_El.innerHTML = playBtnIcons[0];
-        clearInterval(autoplay_interval);
-    }
-}
- */
+}); */
